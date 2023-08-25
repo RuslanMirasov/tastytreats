023f1d1877470af6dda6b4e368f81fbbfd2b664f
RuslanMirasov <126423989+RuslanMirasov@users.noreply.github.com>
2023-08-25 22:54:12 +0000

    Deploying to gh-pages from @ RuslanMirasov/tastytreats@efe72c0422caa5ac173e130c510954cb4065f1c3 🚀

--- index.966c2876.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector(".body"),document.querySelector(".header");console.log(refs.body)}();
-//# sourceMappingURL=index.966c2876.js.map
--- index.f524e162.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".body"),document.querySelector(".header");console.log(refs.body);
-//# sourceMappingURL=index.f524e162.js.map
