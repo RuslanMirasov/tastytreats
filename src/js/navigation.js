a77a17a3ab95303d654a1237f48583165d9c1f28
RuslanMirasov <info@mirasov.dev>
2023-08-28 14:42:27 +0200

    sdsd

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,6 +1,9 @@
 import { refs } from '../js/refs';
+import { scrollbarModify, scrollbarReset } from '../js/popup';
 
-refs.burger.addEventListener('click', menuToggle);
+if (refs.burger) {
+  refs.burger.addEventListener('click', menuToggle);
+}
 
 refs.navigationLinks.forEach(navItem => {
   if (navItem.querySelector('ul')) {
@@ -27,7 +30,27 @@ refs.menuBackdrop.addEventListener('click', e => {
 });
 
 function menuToggle() {
+  scrollbarCheck();
   refs.menuBackdrop.classList.toggle('is-open');
   refs.burger.classList.toggle('is-open');
   refs.body.classList.toggle('locked');
+}
+
+function scrollbarCheck() {
+  if (!refs.body.classList.contains('locked')) {
+    const scrollbarWidth = window.innerWidth - refs.main.offsetWidth;
+    refs.main.paddingRight = `${scrollbarWidth}px`;
+    refs.fixedElements.forEach(fix => {
+      if (!fix.classList.contains('custom-popup')) {
+        fix.style.paddingRight = `${scrollbarWidth}px`;
+      }
+    });
+  } else {
+    refs.main.paddingRight = `0px`;
+    refs.fixedElements.forEach(fix => {
+      if (!fix.classList.contains('custom-popup')) {
+        fix.style.paddingRight = `0px`;
+      }
+    });
+  }
 }
