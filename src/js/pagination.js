9fe78a9a8398e34fbd1834ea7d6eebce4956c624
RuslanMirasov <info@mirasov.dev>
2023-09-02 22:46:47 +0200

    pagination

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -8,8 +8,9 @@ const catalogPagination = new Pagination(paginationEl, {
   template: {
     page: '<a href="#" class="tui-page-btn">{{page}}</a>',
     currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-    moveButton: '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' + 'huy' + '</a>',
-    disabledMoveButton: '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}">' + 'pizda' + '</span>',
-    moreButton: '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}}">' + '...' + '</a>',
+    moveButton: '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' + '<span class="tui-ico-{{type}}">{{type}}</span>' + '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}">' + '<span class="tui-ico-{{type}}">{{type}}</span>' + '</span>',
+    moreButton: '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}}">' + '<span class="tui-ico-ellip">...</span>' + '</a>',
   },
 });
